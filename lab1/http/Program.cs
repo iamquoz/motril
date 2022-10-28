78c49bdb21a621058f2516585fbfa927c38ceb50
iamquoz <fylhtq30rjrjkjd@gmail.com>
2022-10-28 19:56:03 +0400

    lab 1 http and shell

---
+++ lab1/http/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+socket.Bind(new IPEndPoint(IPAddress.Any, 50000));
+
+socket.Listen(0);
+
+var handler = await socket.AcceptAsync();
+
+while (true)
+{
+	var buffer = new byte[65536];
+	var bytesReceived = await handler.ReceiveAsync(buffer, SocketFlags.None);
+	var request = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+	if (request.Contains("GET")) {
+		var page = request.Split(" ").Last().TrimEnd(Environment.NewLine.ToCharArray());
+		Console.WriteLine(page);
+		if (page == "/index.html")
+			buffer = Encoding.UTF8.GetBytes("<html><head></head><body><h1>test</h1></body></html>\n");
+		else
+			buffer = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\n");
+	}
+	else
+	{
+		buffer = Encoding.UTF8.GetBytes("HTTP/1.1 419 Method not supported\n");
+	}
+	await handler.SendAsync(buffer, SocketFlags.None);
+}
---
+++ lab1/shell/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+socket.Bind(new IPEndPoint(IPAddress.Any, 50000));
+
+socket.Listen(0);
+
+var handler = await socket.AcceptAsync();
+
+while (true)
+{
+	var buffer = new byte[65536];
+	var bytesReceived = await handler.ReceiveAsync(buffer, SocketFlags.None);
+	var request = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+	var output = RunNslookup(request);
+	output = output.Remove(output.LastIndexOf(Environment.NewLine));
+	buffer = Encoding.UTF8.GetBytes(output);
+	await handler.SendAsync(buffer, SocketFlags.None);
+}
+
+string RunNslookup(string target)
+{
+	Process cmd = new Process();
+	cmd.StartInfo.FileName = $"nslookup";
+	cmd.StartInfo.RedirectStandardInput = true;
+	cmd.StartInfo.RedirectStandardOutput = true;
+	cmd.StartInfo.CreateNoWindow = true;
+	cmd.StartInfo.UseShellExecute = false;
+	cmd.Start();
+
+	cmd.StandardInput.WriteLine($"{target}");
+	cmd.StandardInput.Flush();
+	cmd.StandardInput.Close();
+
+	var output = cmd.StandardOutput.ReadToEnd();
+	
+	cmd.WaitForExit();
+	
+	return output;
+}
